197c0cf198cc8e6047973c9bcb1915540a78cc82
Krisi <you@example.com>
2021-09-29 14:54:21 +0200

    Projekt kezdés

--- src/hu/petrik/Bejegyzes.java
+++ src/hu/petrik/Bejegyzes.java
@@ -1,5 +1,19 @@
 package hu.petrik;
 
+import java.time.LocalDateTime;
+
 public class Bejegyzes {
     private String  szerzo;
+    private String  tartalom;
+    private int likeok;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+    
+
+
+
 }
+
+
+
