0b34e29199a21d1b799d70cd9559b866360c3f19
Krisi <you@example.com>
2021-09-29 15:47:17 +0200

    Datetime kiírása

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -1,10 +1,29 @@
 package hu.petrik;
 
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        System.out.println(now.format(formatter));
+        LocalDateTime egyik = LocalDateTime.parse("2021-09-25 15:03:15",formatter);
+        LocalDateTime masik = LocalDateTime.parse("2021-09-25 15:03:15",formatter);
+        LocalDateTime harmadik = LocalDateTime.parse("2021-09-25 15:20:15",formatter);
+        if (egyik.equals(masik)){
+            System.out.println("Ugyan az az időpont");
+        }else {
+            System.out.println("Nem");
+        }
+
+        if (egyik.equals(harmadik)){
+            System.out.println("Ugyan az az időpont");
+        }else {
+            System.out.println("Nem egyezik");
+        }
+
     }
 }
