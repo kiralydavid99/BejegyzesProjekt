8074890fc0f7aedc79eab1b81f8a43bf075481f1
Krisi <you@example.com>
2021-09-29 15:00:01 +0200

    Get,set elkészítése

--- src/hu/petrik/Bejegyzes.java
+++ src/hu/petrik/Bejegyzes.java
@@ -9,7 +9,42 @@ public class Bejegyzes {
     private LocalDateTime letrejott;
     private LocalDateTime szerkesztve;
 
-    
+    public Bejegyzes(String szerzo, String tartalom){
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+        this.likeok = 0;
+        this.letrejott = LocalDateTime.now();
+        this.szerkesztve = LocalDateTime.now();
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.szerkesztve = LocalDateTime.now();
+        this.tartalom = tartalom;
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
+    }
+
+    public void like(){
+        this.likeok++;
+    }
 
 
 
